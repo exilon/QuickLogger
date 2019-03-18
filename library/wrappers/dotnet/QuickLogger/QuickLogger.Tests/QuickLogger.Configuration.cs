f30345e9c0423babab2f5027b8806a3d2b06f237
Turrican <djkike@djkike.es>
2019-03-18 14:34:53 +0100

    Organization of tests, added more tests

--- library/wrappers/dotnet/QuickLogger/QuickLogger.Tests/QuickLogger.Configuration.cs
+++ library/wrappers/dotnet/QuickLogger/QuickLogger.Tests/QuickLogger.Configuration.cs
@@ -8,7 +8,7 @@ using QuickLogger.NetStandard.Abstractions;
 
 namespace QuickLogger.Tests.Unit
 {    
-    [TestFixture(Category = "Integration")]    
+    [TestFixture(Category = "Unit")]    
     public class QuickLogger_Configuration_Should
     {
         static string _configPath = String.Empty;
@@ -29,11 +29,18 @@ namespace QuickLogger.Tests.Unit
             return provider;
         }
 
-        public ILoggerConfigManager GetaTestConfigManager(string configpath)
+        public ILoggerConfigManager GetaTestFileConfigManager(string configpath)
         {
             ILoggerConfigManager configmanager = new QuickLoggerFileConfigManager(configpath);
             return configmanager;
         }
+        public ILoggerConfigManager GetTestStringConfigManager()
+        {
+            var conf = "{  \"environment\": \"Test\",  \"providers\": [    {      \"providerProps\": {        \"providerName\": \"A dirty File provider\",        \"providerType\": \"RedisProvider\",        \"providerInfo\": {          \"AppName\": \"Habitatsoft.SignIn.API\",          \"LogLevel\": \"[etHeader,etInfo,etSuccess,etWarning,etError,etCritical,etException,etDebug,etTrace,etDone,etCustom1,etCustom2]\",          \"Host\": \"elksistemas.westeurope.cloudapp.azure.com\",          \"Port\": 6379,          \"Password\": \"\",	            \"LogKey\": \"sistemas-logstash-key\",          \"MaxSize\": 1000,                    \"MaxFailsToRestart\": 2,          \"MaxFailsToStop\": 0,         	        \"OutputAsJson\": true,	        \"Enable\": true        }      }    }  ]}";
+            ILoggerConfigManager configmanager = new QuickLoggerStringConfigManager(conf);
+            return configmanager;
+        }
+
         [OneTimeSetUp]
         public void SetUp()
         {
@@ -50,22 +57,21 @@ namespace QuickLogger.Tests.Unit
         [Test]
         public void Add_Logger_provider_into_a_new_configuration_manager()
         {
-            ILoggerConfigManager configmanager = GetaTestConfigManager(_configPath);
+            ILoggerConfigManager configmanager = GetaTestFileConfigManager(_configPath);
             configmanager.GetSettings().addProvider(GetaTestProvider(_testprovidername));
             Assert.That(configmanager.GetSettings().getProvider(_testprovidername), !Is.Null);
         }
         [Test]
         public void Make_default_settings_and_save_to_disk()
         {
-            ILoggerConfigManager configmanager = GetaTestConfigManager(_configPath);
+            ILoggerConfigManager configmanager = GetaTestFileConfigManager(_configPath);
             configmanager.Write();
             Assert.That(File.Exists(_configPath), Is.True);
         }
-
         [Test]
         public void Create_Save_And_Load_logger_configuration_from_disk()
         {
-            ILoggerConfigManager configmanager = GetaTestConfigManager(_configPath);
+            ILoggerConfigManager configmanager = GetaTestFileConfigManager(_configPath);
             configmanager.GetSettings().addProvider(GetaTestProvider(_testprovidername));
             configmanager.GetSettings().setEnvironment("Test");
             configmanager.Write();
@@ -76,7 +82,15 @@ namespace QuickLogger.Tests.Unit
             Assert.That(configmanager.GetSettings().getEnvironment(), Is.EqualTo("Test"));
             Assert.That(configmanager.GetSettings().getProvider(_testprovidername), !Is.Null);
         }
-
+        [Test]
+        public void Create_logger_configuration_from_string()
+        {
+            ILoggerConfigManager configmanager = GetTestStringConfigManager();
+            configmanager.Load();
+            Assert.That(configmanager.GetSettings(), !Is.Null);
+            Assert.That(configmanager.GetSettings().getEnvironment(), Is.EqualTo("Test"));
+            Assert.That(configmanager.GetSettings().getProvider(_testprovidername), !Is.Null);
+        }
         [TearDown]
         public void TearDown()
         {
